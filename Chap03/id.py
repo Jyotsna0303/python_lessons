ec11a82227204e0019ed468dcb104dd0f1775531
Jyotsna Kukreti <30364057+Jyotsna0303@users.noreply.github.com>
2022-12-21 21:49:36 +0530

    Update id.py

--- Chap03/id.py
+++ Chap03/id.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-#id()
+#id()  return the identity of an object. This identity has to be unique and constant for this object during the lifetime. Two objects with non-overlapping lifetimes may have the same id() value
+# If we relate this to C, then they are actually the memory address, here in Python it is the unique id. This function is generally used internally in Python.
 i = (1,'four', 3.0,[1,'two'])
 j = (1,'four', 3.0,[1,'two'])
 print(id(i))
@@ -25,21 +26,21 @@ print(id(j[1]))
 
  #is operator
 if i[0] is j[0]:
-    print('they are same object')
+    print('they are same object') #true
 
 if i is j:
     print('i and j are same object')
 else:
-    print('i and j  are not same object')
+    print('i and j  are not same object')#true
 
 a= 5
 b=5
 print(id(a))
 print(id(b))
 if a is b:
-    print("a and b are same object")
+    print("a and b are same object") #true
 else:
-    print("a and b are not same object")
+    print("a and b are not same object") 
 
 
 
@@ -47,7 +48,7 @@ else:
 #isinstance() method to check if object of same
 
 if isinstance(i,tuple):
-    print('i is tuple')
+    print('i is tuple') #true
 elif isinstance(i,list):
     print('i is list')
 
